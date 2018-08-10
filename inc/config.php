75e374f4cabb10b1e713ef52377e8ff4b0d87bdd
ak53amod <42261611+ak53amod@users.noreply.github.com>
2018-08-10 12:06:44 +0545

    Add files via upload

--- inc/config.php
+++ inc/config.php
@@ -1,25 +1,25 @@
-<?php 
-const SITE_URL = 'http://goshopping.com/';
+<?php
+const SITE_URL = 'https://advancedprojectak.herokuapp.com/';
 const ADMIN_URL = SITE_URL."/admin/";
 
 define('INC_PATH', $_SERVER['DOCUMENT_ROOT']."/admin/");
 
-const ASSETS_URL = SITE_URL."assets/";
+const ASSETS_URL = ADMIN_URL."assets/";
 const CSS_URL = ASSETS_URL."css/";
 const JS_URL = ASSETS_URL."js/";
-const IMAGES_URL = ASSETS_URL."images/";
+const PLUGINS_URL = ASSETS_URL."plugins/";
+const FONTAWESOME_URL = ASSETS_URL."font-awesome/";
 
-const SITE_NAME = "Go Shopping || Home";
+const SITE_NAME = "Go Shopping || Admin";
 
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
 const DB_PASS = '';
 const DB_NAME = 'goshopping';
 
-const UPLOAD_URL = SITE_URL."uploads/";
+const UPLOAD_URL = SITE_URL."/uploads/";
 const PRODUCT_URL = SITE_URL."/upload/product/";
 define('PRODUCT_DIR', $_SERVER['DOCUMENT_ROOT']."/upload/product/");
-define('UPLOAD_DIR', $_SERVER['DOCUMENT_ROOT']."/uploads/");
 
 $allowed_exts = array('jpeg','jpg','png','gif','bmp');
-$GLOBALS['a'] = array();
+
